8355c0df40496a56ab82c82157c9cde091b546a0
marcofilho <marco.filho@ess.eu>
2024-08-25 21:18:07 +0200

    Add initial test application
    Test application can use simDetectorConfigure function, NDStdArrays
    plugin and also the plugin being developed itself.
    
    I couldn't think of a way to add only modules paths such as BUSY and
    ADSIMDETECTOR only to testApp, but that shouldn't be a problem since
    the libs and dbds are only added in testApp/src/Makefile.
    
    The st.cmd has two stdArrays: one for connecting to the simdetector
    and seeing the raw image and one for connecting to the plugin and seeing
    the image that comes out of it, if any iimage comes out at all.
    
    Remember to keep hydrated.

---
+++ {{ cookiecutter.project_slug }}/iocBoot/ioctest/st.cmd
@@ -0,0 +1,35 @@
+#!../../bin/linux-x86_64/test
+
+#- You may have to change test to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+epicsEnvSet("PREFIX", "TEST:")
+epicsEnvSet("PORT", "SIM")
+epicsEnvSet("PLUGIN_PORT", "{{ cookiecutter.project_name }}")
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(TOP)/db")
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/test.dbd"
+test_registerRecordDeviceDriver pdbbase
+
+simDetectorConfig("$(PORT)", 255, 255, 1, 100, 0)
+dbLoadRecords("$(ADSIMDETECTOR)/db/simDetector.template","P=$(PREFIX),R=cam:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+
+ND{{ cookiecutter.project_name }}Configure("$(PLUGIN_PORT)", 255, 255, 1, 100, 0)
+dbLoadRecords("{{ cookiecutter.project_slug }}.template", "P=$(PREFIX),R={{ cookiecutter.project_name }}:,PORT={{ cookiecutter.project_name }},ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT)")
+
+NDStdArraysConfigure("Image1", 20, 0, "$(PORT)", 0, 0, 0, 0, 0, 5)
+dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int8,FTVL=UCHAR,NELEMENTS=12000000")
+
+NDStdArraysConfigure("Image2", 20, 0, "$(PORT)", 0, 0, 0, 0, 0, 5)
+dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=image2:,PORT=Image2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PLUGIN_PORT),TYPE=Int8,FTVL=UCHAR,NELEMENTS=12000000")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=marco"
